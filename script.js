1ebc25c608f1e2ec53169e6a68f90fb89e0777b7
Zetaphor <zetaphor@gmail.com>
2020-05-19 01:26:31 -0400

    Menu reset and overhead target changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let camera, scene, renderer, container;
-let conLeft, conRight, xrConLeft, xrConRight, rayLeft, rayRight, headCube
+let conLeft, conRight, xrConLeft, xrConRight, rayLeft, rayRight, overheadTarget
 let light, debugDisplay, INTERSECTED;
 let tempMatrix = new THREE.Matrix4();
 const menuHand = 'left';
@@ -9,8 +9,8 @@ const debug = true;
 let entranceStarted = false;
 const entranceDuration = 500;
 let entranceTween;
-let startScale = { x: 0.1, y: 0.1, z: 0.1 };
-let finalScale = { x: 1, y: 1, z: 1 };
+const startScale = { x: 0.1, y: 0.1, z: 0.1 };
+const finalScale = { x: 1, y: 1, z: 1 };
 let currentScale = { x: 0.1, y: 0.1, z: 0.1 };
 
 init();
@@ -29,9 +29,10 @@ function init() {
   light = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
   scene.add(light);
 
-  headCube = new THREE.Mesh(new THREE.BoxGeometry(2, 0.5, 1), new THREE.MeshLambertMaterial({ color: 0x00ff00 }));
-  headCube.name = 'headCube'
-  scene.add(headCube);
+  overheadTarget = new THREE.Mesh(new THREE.CylinderGeometry(0.15, 0.15, 0.01), new THREE.MeshLambertMaterial({
+      color: 0xffffff, transparent: true, opacity: 0.4 }));
+  overheadTarget.name = 'overheadTarget';
+  scene.add(overheadTarget);
 
   conLeft = new THREE.Mesh(new THREE.BoxGeometry(0.1, 0.1, 0.1), new THREE.MeshLambertMaterial({ color: 0xff0000 }));
   conRight = new THREE.Mesh(new THREE.BoxGeometry(0.1, 0.1, 0.1), new THREE.MeshLambertMaterial({ color: 0x0000ff }));
@@ -44,7 +45,7 @@ function init() {
   scene.add(menuBase);
 
   if (!debug) {
-    headCube.visible = false;
+    overheadTarget.visible = false;
     conLeft.visible = false;
     conRight.visible = false;
   }
@@ -99,11 +100,22 @@ function render() {
   conRight.rotation.y = xrConRight.rotation.y;
   conRight.rotation.z = xrConRight.rotation.z;
 
+  if (menuHand === 'left') {
+    overheadTarget.position.x = xrConLeft.position.x;
+    overheadTarget.position.y = xrConLeft.position.y + 0.3;
+    overheadTarget.position.z = xrConLeft.position.z;
+  } else {
+    overheadTarget.position.x = xrConRight.position.x;
+    overheadTarget.position.y = xrConRight.position.y + 0.3;
+    overheadTarget.position.z = xrConRight.position.z;
+  }
+
   if (menuRayActive) {
     if (menuHand === 'left') {
       menuBase.position.x = xrConLeft.position.x;
       menuBase.position.y = xrConLeft.position.y + 0.07;
       menuBase.position.z = xrConLeft.position.z;
+
     } else {
       menuBase.position.x = xrConRight.position.x;
       menuBase.position.y = xrConRight.position.y + 0.07;
@@ -113,23 +125,11 @@ function render() {
 
   checkMenuRay(menuHand);
 
-  let cameraRot = new THREE.Quaternion().setFromRotationMatrix(camera.matrixWorld);
-  let cameraPos = new THREE.Vector3().setFromMatrixPosition(camera.matrixWorld);
-  camera.updateMatrixWorld(true);
-
-  headCube.position.x = cameraPos.x;
-  headCube.position.y = cameraPos.y + 0.5;
-  headCube.position.z = cameraPos.z;
-  headCube.position.z = cameraPos.z - 0.5;
-
   if (debug) {
     let rotString = `
-    <h3>Camera</h3>
-    <p>Camera Position: ${cameraPos.x.toFixed(3)}, ${cameraPos.y.toFixed(3)}, ${cameraPos.z.toFixed(3)}</p>
-    <p>Camera Rotation: ${cameraRot._x.toFixed(3)}, ${cameraRot._y.toFixed(3)}, ${cameraRot._z.toFixed(3)}</p>
-    <h3>Cube</h3>
-    <p>Cube Position: ${headCube.position.x.toFixed(3)}, ${headCube.position.y.toFixed(3)}, ${headCube.position.z.toFixed(3)}</p>
-    <p>Cube Rotation: ${headCube.rotation.x.toFixed(3)}, ${headCube.rotation.y.toFixed(3)}, ${headCube.rotation.z.toFixed(3)}</p>
+    <h3>Overhead Target</h3>
+    <p>Position: ${overheadTarget.position.x.toFixed(3)}, ${overheadTarget.position.y.toFixed(3)}, ${overheadTarget.position.z.toFixed(3)}</p>
+    <p>Rotation: ${overheadTarget.rotation.x.toFixed(3)}, ${overheadTarget.rotation.y.toFixed(3)}, ${overheadTarget.rotation.z.toFixed(3)}</p>
     `;
 
     debugDisplay.innerHTML = rotString;
@@ -148,13 +148,15 @@ function checkMenuRay() {
   if (menuHand === 'left') controllerRay.ray.direction.set(1, 0, 0).applyMatrix4(tempMatrix);
   else controllerRay.ray.direction.set(-1, 0, 0).applyMatrix4(tempMatrix);
 
-  let intersects = controllerRay.intersectObjects([headCube]);
+  let intersects = controllerRay.intersectObjects([overheadTarget]);
   if (intersects.length > 0) {
     if (menuHand === 'left') conLeft.material.emissive.setHex(0x0000ff);
     if (menuHand === 'right') conRight.material.emissive.setHex(0xff0000);
 
     if (!menuRayActive) {
       menuBase.visible = true;
+      menuBase.scale.set(startScale.x, startScale.y, startScale.z);
+      currentScale = startScale
       menuRayActive = true;
     }
 
@@ -164,12 +166,13 @@ function checkMenuRay() {
       // menuGroup.scale.set(0.1, 0.1, 0.1);
       console.log('Entrance Started')
       entranceStarted = true;
-      menuBase.scale.set(startScale.x, startScale.y, startScale.z);
-      currentScale = startScale
-      // console.log(currentScale)
-      entranceTween = new TWEEN.Tween(currentScale)
+      currentScale = {x: 0.1, y: 0.1, z: 0.1};
+      menuBase.scale.set(0.1, 0.1, 0.1);
+      TWEEN.removeAll();
+
+      new TWEEN.Tween(currentScale)
         .to(finalScale, entranceDuration)
-        .easing(TWEEN.Easing.Exponential.In)
+        .easing(TWEEN.Easing.Sinusoidal.In)
         .onUpdate(() => {
           menuBase.scale.set(currentScale.x, currentScale.y, currentScale.z);
         }).start();
@@ -179,7 +182,7 @@ function checkMenuRay() {
       menuBase.visible = false;
       menuRayActive = false;
       entranceStarted = false;
-      entranceTween.stop();
+      // entranceTween.stop();
     }
     if (menuHand === 'left') conLeft.material.emissive.setHex(0x000000);
     if (menuHand === 'right') conRight.material.emissive.setHex(0x000000);
